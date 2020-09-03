27dd2767a8bd6471832ffdb5b6b9038ddaad0521
Ojknation <ojknation@gmail.com>
2020-09-03 23:28:20 +0100

    updated footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -3,7 +3,7 @@ import { Grid, ListItem, Box, List } from '@chakra-ui/core'
 
 const Footer = () => {
     return (
-        <Grid templateColumns="repeat(12, 1fr)" backgroundColor="primary" color="white">
+        <Grid templateColumns="repeat(12, 1fr)" backgroundColor="primary" color="white" mt={{md:"350px", lg:"100px"}}>
             <Box gridColumn={{sm:"2/12", md:"1/4"}} p={20}>
                 <List>
                     <ListItem>
